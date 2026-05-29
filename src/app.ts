0c1c6f46155fb06b14a9432b15bdbd613af7832a
Tnsae <tinsaemk6679@gmail.com>
2026-05-29 23:57:31 +0300

    chore: introduced rate limiter

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from "express";
 import helmet from "helmet";
 import cors from "cors";
+import limiter from "./utils/rate.limiter";
 import authRoutes from "./routes/auth.routes";
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(cors()); // or cors({ origin: "http://localhost:3000" }) for specific origin of frontend
+app.use(limiter);
 //defined routes
 app.use("/api/auth", authRoutes);
 
