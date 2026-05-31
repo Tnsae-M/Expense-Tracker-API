2ce901b39f2d45df96901d5a05ab97468b57a2d1
Tnsae <tinsaemk6679@gmail.com>
2026-05-31 13:31:00 +0300

    feat: added an auth middleware

---
+++ src/middleware/auth.guard.ts
@@ -0,0 +1,38 @@
+import { verifyToken } from "../utils/token";
+import { Request, Response, NextFunction } from "express";
+export async function protectedRoute(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    let token: string | undefined;
+    if (req.cookies && req.cookies.accessToken) {
+      token = req.cookies.accessToken;
+    }
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        error: "Not authorized, token missing!",
+      });
+    }
+    const decodedLoad = await verifyToken(token);
+    if (!decodedLoad) {
+      return res.status(401).json({
+        success: false,
+        error: "Not authorized, invalid or expired token!",
+      });
+    }
+    req.user = {
+      userId: decodedLoad.userId,
+      email: decodedLoad.email,
+    };
+    next();
+  } catch (er) {
+    console.log("auth middleware error: ", er);
+    return res.status(500).json({
+      success: false,
+      error: "Internal server auth error",
+    });
+  }
+}
---
+++ src/types/express.req.d.ts
@@ -0,0 +1,12 @@
+import { TokenPayload } from "../utils/token";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: number;
+        email: string;
+      };
+    }
+  }
+}
