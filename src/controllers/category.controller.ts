fa9d463300a018e29f70c783968f99f9e215adad
Tnsae <tinsaemk6679@gmail.com>
2026-06-01 14:42:04 +0300

    feat: added get all,update,and delete category(ies) endpoints

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { createCategory } from "../services/category.service";
+import {
+  createCategory,
+  getAllCategories,
+  updateCategory,
+  deleteCategory,
+} from "../services/category.service";
 import { catchAsync } from "../utils/catch.async";
 
 export const NewCategory = catchAsync(async (req: Request, res: Response) => {
@@ -10,3 +15,36 @@ export const NewCategory = catchAsync(async (req: Request, res: Response) => {
     data: newCategory,
   });
 });
+export const GetAllCategories = catchAsync(
+  async (req: Request, res: Response) => {
+    const categories = await getAllCategories();
+    res.status(200).json({
+      success: true,
+      message: "categories retrieved successfully",
+      data: categories,
+    });
+  },
+);
+export const UpdateCategory = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    req.body.id = Number(id);
+    const updatedCategory = await updateCategory(req.body);
+    res.status(200).json({
+      success: true,
+      message: "category updated successfully",
+      data: updatedCategory,
+    });
+  },
+);
+export const DeleteCategory = catchAsync(
+  async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const deletedCategory = await deleteCategory(id);
+    res.status(200).json({
+      success: true,
+      message: "category deleted successfully",
+      data: deletedCategory,
+    });
+  },
+);
--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -21,4 +21,37 @@ async function createCategory(data: CategoryModel): Promise<CategoryModel> {
   });
   return newCategory;
 }
-export { createCategory };
+async function getAllCategories(): Promise<CategoryModel[]> {
+  const categories = await prisma.category.findMany();
+  if (!categories) {
+    throw new appError("no categories found", 404);
+  }
+  return categories;
+}
+async function updateCategory(data: CategoryModel): Promise<CategoryModel> {
+  const { id, name } = data;
+  if (!id || !name) {
+    throw new appError("missing id or name field", 400);
+  }
+  const updatedCategory = await prisma.category.update({
+    where: {
+      id: id,
+    },
+    data: {
+      name: name,
+    },
+  });
+  return updatedCategory;
+}
+async function deleteCategory(id: number) {
+  if (!id) {
+    throw new appError("missing id field", 400);
+  }
+  const deletedCategory = await prisma.category.delete({
+    where: {
+      id: id,
+    },
+  });
+  return deletedCategory;
+}
+export { createCategory, getAllCategories, updateCategory, deleteCategory };
