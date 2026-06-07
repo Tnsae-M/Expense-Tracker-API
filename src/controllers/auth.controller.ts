d0af2ac8463e98884dc247460686e0a5883b8e4b
Tnsae <tinsaemk6679@gmail.com>
2026-06-07 12:59:52 +0300

    fix(auth): fixed deployment bug on register user

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,13 @@
 import { registerUser, loginUser } from "../services/auth.service";
 import { Request, Response } from "express";
+import { signInSchema, signUpSchema } from "../schemas/user.schema";
 import { generateToken } from "../utils/token";
 import { authReqLimiter } from "../utils/rate.limiter";
 import { catchAsync } from "../utils/catch.async";
 import rateLimit from "express-rate-limit";
 export const signUp = catchAsync(async (req: Request, res: Response) => {
-  const newUser = await registerUser(req.body);
+  const validRequestBody = signUpSchema.parse(req.body);
+  const newUser = await registerUser(validRequestBody);
   const accessToken = await generateToken(newUser.id, newUser.email);
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
